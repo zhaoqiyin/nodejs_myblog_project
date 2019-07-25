f5e3abed2aaa41dd7ecf5efe0ed12cf5a4b95dbd
yinzhaoqi <yinzhaoqi@126.com>
2019-07-25 16:49:52 +0800

    增加html_test， 联调登录

--- blog_1/src/conf/db.js
+++ blog_1/src/conf/db.js
@@ -1,28 +1,42 @@
- const env = process.env.NODE_ENV  // 环境参数
+const env = process.env.NODE_ENV  // 环境参数
 
- // 配置
- let MYSQL_CONF
+// 配置
+let MYSQL_CONF
+let REDIS_CONF
 
- if (env === 'dev') {
-    MYSQL_CONF = {
-        host: 'localhost',
-        user: 'root',
-        password: '123',
-        port: '3306',
-        database: 'myblog'
-    }
- }
+if (env === 'dev') {
+   MYSQL_CONF = {
+      host: 'localhost',
+      user: 'root',
+      password: '123',
+      port: '3306',
+      database: 'myblog'
+   }
 
- if (env === 'production') {
-    MYSQL_CONF = {
-        host: 'localhost',
-        user: 'root',
-        password: '123',
-        port: '3306',
-        database: 'myblog'
-    }
- }
+   // redis
+   REDIS_CONF = {
+      port: 6379,
+      host: '127.0.0.1'
+   }
+}
 
- module.exports = {
-    MYSQL_CONF
- }
+if (env === 'production') {
+   MYSQL_CONF = {
+      host: 'localhost',
+      user: 'root',
+      password: '123',
+      port: '3306',
+      database: 'myblog'
+   }
+
+   // redis
+   REDIS_CONF = {
+      port: 6379,
+      host: '127.0.0.1'
+   }
+}
+
+module.exports = {
+   MYSQL_CONF,
+   REDIS_CONF
+}
--- blog_1/src/router/blog.js
+++ blog_1/src/router/blog.js
@@ -6,6 +6,16 @@
      delBlog
     } = require('../controller/blog')
  const  { SuccessModel, ErrorModel } = require('../model/resModel')
+
+ // 统一的登录验证函数
+ const loginCheck = (req) => {
+    if (!req.session.username) {
+        return Promise.resolve(
+            new ErrorModel('尚未登录')
+        ) 
+    }
+    
+ }
  
  const handleBlogRouter = (req, res) => {
     const method = req.method
@@ -13,10 +23,22 @@
 
     // 获取博客列表
     if (method === 'GET' && req.path  === '/api/blog/list') {
-        const author = req.query.author || ''
+        let author = req.query.author || ''
         const keyword = req.query.keyword || ''
         // const listData = getList(author, keyword)
         // return new SuccessModel(listData)
+
+        if (req.query.isadmin) {
+            // 管理员界面
+            const loginCheckResult = loginCheck(req)
+            if (loginCheckResult) {
+                // 未登录
+                return loginCheckResult
+            }
+            // 强制查询自己的博客
+            author = req.session.username
+        }
+
         const result = getList(author, keyword)
         return  result.then(listData => {
             return new SuccessModel(listData)
@@ -38,7 +60,13 @@
         // const data = newBlog(req.body)
         // return new SuccessModel(data)
 
-        req.body.author = 'zhangsan' // 假数据，待开发登录时再改成真实数据
+        const loginCheckResult = loginCheck(req)
+        if (loginCheckResult) {
+            // 未登录
+            return loginCheckResult
+        }
+
+        req.body.author = req.session.username
         const result = newBlog(req.body)
         return result.then(data => {
             return new SuccessModel(data)
@@ -46,7 +74,13 @@
     }
 
     // 更新一篇博客
-    if (method === 'POST' && req.path === '/api/blog/update') { 
+    if (method === 'POST' && req.path === '/api/blog/update') {
+        const loginCheckResult = loginCheck(req)
+        if (loginCheckResult) {
+            // 未登录
+            return loginCheckResult
+        }
+
         const result = updateBlog(id, req.body)
         return result.then(val => {
             if (val) {
@@ -59,7 +93,13 @@
 
     // 删除一篇博客
     if (method === 'POST' && req.path === '/api/blog/del') {
-        const author = 'zhangsan' // 假数据，待开发登录时再改成真实数据
+        const loginCheckResult = loginCheck(req)
+        if (loginCheckResult) {
+            // 未登录
+            return loginCheckResult
+        }
+
+        const author = req.session.username
         const result = delBlog(id, author)
         return result.then(val => {
             if (val) {
